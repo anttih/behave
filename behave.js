af550bb16093d64bb72c88c441a8efbac3a5e3de
Antti Holvikari <anttih@gmail.com>
2011-06-14 22:25:00 +0300

    Extract context to its own prototype TestContext

--- behave.js
+++ behave.js
@@ -10,40 +10,37 @@ var Runner = exports.Runner = function() {
 
 Runner.prototype = {
     run: function (suite) {
-        this._run_context([this.top_level_context], suite);
+        var context = new TestContext([this.top_level_context]);
+        this._run_context(context, suite);
     },
 
     _run_context: function (context, suite) {
-        var before_each, context_copy, name, test;
+        var name, test;
+
         if ('before_each' in suite) {
-            before_each = suite.before_each;
+            context.before_each = suite.before_each;
         }
+
         for (name in suite) {
             test = suite[name];
             
             if (typeof test === 'object') {
-                context_copy = context.slice(0);
-                context_copy.push(name);
-                this._run_context(context_copy, test);
+                this._run_context(context.push(name), test);
             } else if (typeof test === 'function' && name !== 'before_each') {
-                this._run_test(context, name, test, before_each);
+                this._run_test(context, name, test);
             }
         }
     },
 
-    _run_test: function (context, name, test, before_each) {
-        var that = {};
+    _run_test: function (context, name, test) {
         try {
-            if (before_each) {
-                before_each();
-            }
-            test.apply(that);
-            this.fire('ok', context, name);
+            context.run_test(test);
+            this.fire('ok', context.stack, name);
         } catch (e) {
             if (e.name === 'AssertionError') {
-                this.fire('failure', context, name, e);
+                this.fire('failure', context.stack, name, e);
             } else {
-                this.fire('error', context, name, e);
+                this.fire('error', context.stack, name, e);
             }
         }
     },
@@ -59,5 +56,27 @@ Runner.prototype = {
 
     on: function(event, callback) {
         this.events[event].push(callback);
+    }
+};
+
+var TestContext = function (stack) {
+    // context name stack
+    this.stack = stack;
+    this.before_each = null;
+};
+
+TestContext.prototype = {
+    push: function (name) {
+        var copy = this.stack.slice(0);
+        copy.push(name);
+        return new TestContext(copy);
+    },
+
+    run_test: function (test) {
+        var that = {};
+        if (this.before_each) {
+            this.before_each();
+        }
+        test.apply(that);
     }
 };
