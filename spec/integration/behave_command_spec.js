917ca4a5ac2a66dc3b554fd4b0f0ea90d054ace1
Antti Holvikari <anttih@gmail.com>
2011-06-15 22:51:35 +0300

    Added behave command and first integration test

---
+++ spec/integration/behave_command_spec.js
@@ -0,0 +1,22 @@
+var vows = require('vows');
+var assert = require('assert');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+var CMD = "NODE_PATH=" + process.cwd() + " " + process.cwd() + '/bin/behave';
+
+vows.describe("Behave command").addBatch({
+    'running without path ': {
+        topic: function () {
+            var data = "describe('Context', function () {"
+                     + " it('test name', function () {}); "
+                     + "});";
+
+            fs.writeFileSync("spec/integration/data/spec/test_spec.js", data);
+            exec(CMD, {cwd: __dirname + '/data'}, this.callback);
+        },
+        "runs tests in spec/": function (err, stdout, stderr) {
+            assert.equal(stdout, '\n[TOP-LEVEL] Context\n- test name\n');
+        }
+    }
+}).export(module);
