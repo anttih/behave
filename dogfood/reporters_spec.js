e197bb77a1ecb5b0e3bbb4277022d798254e9591
Antti Holvikari <anttih@gmail.com>
2011-06-16 17:11:48 +0300

    Now dogfooding the whole reporting spec

--- dogfood/reporters_spec.js
+++ dogfood/reporters_spec.js
@@ -12,29 +12,23 @@ describe('Reporters', function () {
         };
     }
 
-    describe('Passing tests', function () {
-        var stream, reporter;
-        before_each(function () {
-            stream = new_stream();
-            reporter = new Reporter(stream);
-        });
+    var stream, reporter;
+    before_each(function () {
+        stream = new_stream();
+        reporter = new Reporter(stream);
+    });
 
+    describe('Passing tests', function () {
         it('prints context as topic', function () {
             reporter.ok(['Topic'], 'test name');
             assert.equal(stream.data, '\nTopic\n- test name\n');
         });
         it('prints context name only once for all the tests in that context', function () {
-            stream = new_stream();
-            var reporter = new Reporter(stream);
-
             reporter.ok(['Topic'], 'test name');
             reporter.ok(['Topic'], 'second test');
             assert.equal(stream.data, '\nTopic\n- test name\n- second test\n');
         });
         it('concatenates nested context names as one', function () {
-            stream = new_stream();
-            var reporter = new Reporter(stream);
-
             reporter.ok(['Topic', 'name'], 'test name');
             assert.equal(stream.data, '\nTopic name\n- test name\n');
         });
@@ -43,28 +37,33 @@ describe('Reporters', function () {
             it('prints example counts', function () {
                 reporter.ok(['Topic'], 'test name');
                 reporter.summary();
-                assert.equal(stream.data, '\nTopic\n- test name\n\n1 examples, 0 failures, 0 errors\n');
+                assert.equal(
+                    stream.data,
+                    '\nTopic\n- test name\n\n1 examples, 0 failures, 0 errors\n'
+                );
             });
         });
     });
 
     describe('Failing test', function () {
         it('prints failure name and message', function () {
-            stream = new_stream();
-            var reporter = new Reporter(stream);
+            reporter.failure(['Topic'], 'test name', new AssertionError({
+                message: 'Message',
+                expected: 'some',
+                actual: 'other'
+            }));
 
-            reporter.failure(['Topic'], 'test name', new AssertionError({message: 'Message'}));
-            assert.equal(stream.data, '\nTopic\nFAIL: Message in test name\n');
+            assert.equal(
+                stream.data,
+                '\nTopic\n- test name\n  Failure: Message\n    expected: "some"\n    got:      "other"\n'
+            );
         });
     });
 
     describe('Erroring test', function () {
         it('prints error name and message', function () {
-            stream = new_stream();
-            var reporter = new Reporter(stream);
-
             reporter.error(['Topic'], 'test name', new Error('Message'));
-            assert.equal(stream.data, '\nTopic\nERROR: Message in test name\n');
+            assert.equal(stream.data, '\nTopic\n- test name\n  Error: Message\n');
         });
     });
 });
